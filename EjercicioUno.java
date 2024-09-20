16bca7c0e31ae4ca06216e4e7361ca00accc88c3
lara-gs <lara_989@hotmail.com>
2024-09-20 11:32:54 +0200

    Este es el ejercicio uno

---
+++ EjercicioUno.java
@@ -0,0 +1,12 @@
+public class EjercicioUno {
+public static void main(String[] args) {
+    
+    int numfav = 13;
+    String name = "Agustine";
+
+  
+    System.out.println("Hola mi nombre es " + name + " y mi numero de la suerte es el " + numfav);
+    
+    
+}
+}
