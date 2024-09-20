721c20d2b1514cc109d253126a01f53fecbbb7b5
lara-gs <lara_989@hotmail.com>
2024-09-20 11:37:37 +0200

    Declarar e inicializar variables

---
+++ EjercicioDos.java
@@ -0,0 +1,13 @@
+public class EjercicioDos {
+    public static void main(String[] args) {
+        int numfav = 13;
+        int edad = 19;
+        
+        numfav = numfav + edad; 
+        edad = numfav - edad;
+        numfav = numfav - edad;
+
+        System.out.println(edad);
+        System.out.println(numfav);
+    }
+}
