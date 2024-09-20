5a4debc94e737a2e23e5beed08b9737c4a9ea804
lara-gs <lara_989@hotmail.com>
2024-09-20 14:52:48 +0200

    Ejercicio sobre XOR

---
+++ EjercicioCuatro.java
@@ -0,0 +1,17 @@
+public class EjercicioCuatro {
+    public static void main(String[] args) {
+        int a = 10;
+        int x = 8;
+        a = a ^ x;
+        System.out.println(a);
+        System.out.println(x);
+        x = a ^ x;
+        System.out.println(a);
+        System.out.println(x);
+        a = a ^ x;
+        System.out.println(a);
+        System.out.println(x);
+
+
+    }
+}
