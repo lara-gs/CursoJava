63242b1f79260629cbe656139513e814425d7325
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 18:26:33 +0200

    Calculadora usando switch

---
+++ EjercicioDiecisiete.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class EjercicioDiecisiete {
+    public static void main(String[] args) {
+        String[] opciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+        int opcion = JOptionPane.showOptionDialog(null, "Selecciona una operación", "Calculadora",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, opciones[0]);
+
+        try {
+            
+            String input1 = JOptionPane.showInputDialog("Ingresa el primer número:");
+            double num1 = Double.parseDouble(input1);
+
+            String input2 = JOptionPane.showInputDialog("Ingresa el segundo número:");
+            double num2 = Double.parseDouble(input2);
+
+            double resultado = 0;
+
+            
+            switch (opcion) {
+                case 0:
+                    resultado = num1 + num2;
+                    break;
+                case 1:
+                    resultado = num1 - num2;
+                    break;
+                case 2:
+                    resultado = num1 * num2;
+                    break;
+                case 3:
+                    resultado = num1 / num2;
+                    break;
+            }
+
+            
+            JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+
+        } catch (NumberFormatException e) {
+            
+    
+        }
+    }
+}
