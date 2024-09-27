964b0507fc98cc02983d14c10cd4472cd9035495
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 19:15:07 +0200

    Dibujo patron piramidal

---
+++ EjercicioVeinticinco.java
@@ -0,0 +1,13 @@
+public class EjercicioVeinticinco {
+    public static void main(String[] args) {
+
+        int filas = 4; 
+
+        for (int i = 1; i <= filas; i++) {
+            for (int x = 1; x <= i; x++) {
+                System.out.print("*"); 
+            }
+        System.out.println(); 
+        }
+    }
+}
