51dcdbbc592a13306751769fe0c312f8b41021d6
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 18:18:42 +0200

    Generar numeros aleatorios usando Math.

---
+++ EjercicioDoce.java
@@ -0,0 +1,8 @@
+public class EjercicioDoce {
+    public static void main(String[] args) {
+        
+        int random = (int) (Math.random() * 100) + 1;
+
+        System.out.println("El numero aleatorio es " + random);
+    }
+}
