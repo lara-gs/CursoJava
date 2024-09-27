1dca45d95c1877478bdb19f48631371d8a39fb87
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 18:44:16 +0200

    Uso de AND y OR

---
+++ EjercicioVeinte.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EjercicioVeinte {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = 0;
+        System.out.println("Introduce un numero ");
+        num = scanner.nextInt();
+
+        if(num>=0 && num%2==0) {
+        System.out.println("Tu numero es entero y par");
+
+        }else if(num>=0 || !(num%3==0)){
+        System.out.println("Tu numero es negativo o impar ");
+        }
+        scanner.close();
+
+
+
+    }
+    
+}
