6e791ebd3a15373c440367def1e1eb520177bde6
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 18:19:51 +0200

    Simulador tirada de dados

---
+++ EjercicioTrece.java
@@ -0,0 +1,10 @@
+public class EjercicioTrece {
+
+    public static void main(String[] args) {
+        
+        int dado1 = (int) (Math.random() *6) +1;
+        int dadoS = (int) (Math.random() *6) +1;
+        System.out.println("El resultado de tus dados es " + dado1 + " y " + dadoS + ".");
+    }
+    
+}
