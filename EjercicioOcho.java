1a5f301ce0ff34cd01e1c6692c0925448644a8ed
Lara Gutierrez <lara_989@hotmail.com>
2024-09-27 17:22:05 +0200

    Calculadora GUI

--- EjercicioOcho.java
+++ EjercicioOcho.java
@@ -5,14 +5,16 @@ public class EjercicioOcho {
 
 	public static void main(String[] args)
 	{
-		int radio;
+		double radio;
 
 
-        Scanner entrada = new Scanner( System.in );
+        Scanner entrada = new Scanner(System.in);
         System.out.print("Introduce el radio del círculo: ");
-        radio = entrada.nextInt();
-        System.out.printf("El diametro del círculo es: %f. ", (Math.PI * radio * radio ) );
+        radio = entrada.nextDouble();
+        System.out.printf("El area del círculo es: %f. ", (Math.PI * radio * radio ));
         // usamos %f porque esperamos que el valor a poner nos lo dira la siguiente cadena, o eso es lo que he entendido
+
+        entrada.close();
 }
 
 }
