6c24ccb5270aad9b35a5f36c1d259e9096ca853c
Lara Gutierrez <lara_989@hotmail.com>
2024-09-28 15:29:20 +0200

    Valores maximos y minimos de Integer, Double y Character

---
+++ EjercicioTreintaicuatro.java
@@ -0,0 +1,11 @@
+public class EjercicioTreintaicuatro {
+    public static void main(String[] args) {
+        System.out.println("Este es el valor maximo que 'Integer' puede guardar " + Integer.MAX_VALUE);
+        System.out.println("Este es el valor minimo que 'Integer' puede guardar " + Integer.MIN_VALUE);
+        System.out.println("Este es el valor maximo que 'Double' puede guardar " + Double.MAX_VALUE);
+        System.out.println("Este es el valor minimo que 'Double' puede guardar " + Double.MIN_VALUE );
+        System.out.println("Este es el valor numerico maximo que 'Character' puede guardar  " + (int)Character.MAX_VALUE);
+        System.out.println("Este es el valor numerico minimo que 'Character' puede guardar " + (int)Character.MIN_VALUE);
+
+    }
+}
